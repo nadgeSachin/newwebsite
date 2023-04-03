6a214797b80769a182a69a939d1a865ff5b920b8
Errorpat-404 <nadgesachin644@gmail.com>
2023-04-03 12:13:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <h2>Hello Pat</h2>
-    </div>
+    <>
+      
+    </>
   );
 }
 
