bf1bde416f4ea246ea37723522aebc1a56d42fe5
Errorpat-404 <nadgesachin644@gmail.com>
2023-04-03 12:23:35 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ function App() {
   return (
     <>
       <h2>Hello Pat What are you doing my love</h2>
+      <header>
+        <ul>
+          <li><a>Home</a></li>
+          <li><a>About</a></li>
+          <li><a>Contact</a></li>
+          <li><a>Services</a></li>
+        </ul>
+      </header>
     </>
   );
 }
